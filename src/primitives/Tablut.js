630f47e68b12f44b0c152905af57b493b797d423
João Loureiro <joao.a.g.loureiro@gmail.com>
2015-12-26 19:29:38 +0000

    Created the Tablut primitive

---
+++ src/primitives/Tablut.js
@@ -0,0 +1,54 @@
+function Tablut(scene) {
+  CGFobject.call(this, scene);
+
+  this.scene = scene;
+
+  this.mainBoard = new MainBoard(scene);
+
+  this.pieces = [];
+
+  this.pieces.push(new King(scene,4,4));
+
+  this.pieces.push(new Swedish(scene,2,4));
+  this.pieces.push(new Swedish(scene,3,4));
+  this.pieces.push(new Swedish(scene,5,4));
+  this.pieces.push(new Swedish(scene,6,4));
+  this.pieces.push(new Swedish(scene,4,2));
+  this.pieces.push(new Swedish(scene,4,3));
+  this.pieces.push(new Swedish(scene,4,5));
+  this.pieces.push(new Swedish(scene,4,6));
+
+  this.pieces.push(new Moscovite(scene,3,0));
+  this.pieces.push(new Moscovite(scene,4,0));
+  this.pieces.push(new Moscovite(scene,5,0));
+  this.pieces.push(new Moscovite(scene,4,1));
+  this.pieces.push(new Moscovite(scene,3,8));
+  this.pieces.push(new Moscovite(scene,4,8));
+  this.pieces.push(new Moscovite(scene,5,8));
+  this.pieces.push(new Moscovite(scene,4,7));
+  this.pieces.push(new Moscovite(scene,0,3));
+  this.pieces.push(new Moscovite(scene,0,4));
+  this.pieces.push(new Moscovite(scene,0,5));
+  this.pieces.push(new Moscovite(scene,1,4));
+  this.pieces.push(new Moscovite(scene,8,3));
+  this.pieces.push(new Moscovite(scene,8,4));
+  this.pieces.push(new Moscovite(scene,8,5));
+  this.pieces.push(new Moscovite(scene,7,4));
+}
+
+Tablut.prototype = Object.create(CGFobject.prototype);
+Tablut.prototype.constructor = Tablut;
+
+Tablut.prototype.display = function() {
+  this.mainBoard.display();
+
+	for (i=0; i<this.pieces.length; i++) {
+		this.scene.registerForPick(i+1, this.pieces[i]);
+		this.pieces[i].display();
+	}
+
+};
+
+Tablut.prototype.setTextureAmplification = function(amplifS, amplifT) {
+  this.cell.setTextureAmplification(amplifS, amplifT);
+};
