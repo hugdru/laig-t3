f82cabe6a370e595721c7ced058cfabfbf58e1b8
João Loureiro <joao.a.g.loureiro@gmail.com>
2015-12-28 19:19:00 +0000

    Fixed the name of the files, they were wrong

--- src/primitives/auxiliary/Swede.js
+++ src/primitives/auxiliary/Swede.js
@@ -1,9 +1,6 @@
-function Cell(scene, x, y) {
-  NURBSPlane.call(this, scene, 10);
-
-  this.x = x;
-  this.y = y;
+function Swede(scene, x, y) {
+  Pawn.call(this, scene, x, y);
 }
 
-Cell.prototype = Object.create(NURBSPlane.prototype);
-Cell.prototype.constructor = Cell;
+Swede.prototype = Object.create(Pawn.prototype);
+Swede.prototype.constructor = Swede;
